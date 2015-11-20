e8555e27e7d24a91da584bf622e63904d68498c5
cliff0891 <yuyunc@student.swosu.edu>
2015-11-20 15:07:33 -0600

    commit

--- src/sort3numbers.java
+++ src/sort3numbers.java
@@ -1,46 +1,91 @@
+
+
 import java.util.Scanner;
-public class sort3numbers {
+import java.util.Arrays;
+
 	
-	public static void main(String[] args) {
-System.out.println("Hello class, this is a sorting algorithm using arrays.");
-		
-		Scanner userInput = new Scanner(System.in);
-		
-		System.out.println("Enter 3 numbers:");
-		
-		System.out.print("Your first number:");
-		int n1 = userInput.nextInt();
-		
-		System.out.print("Your second number:");
-		int n2 = userInput.nextInt();
-		
-		System.out.print("Your third number:");
-		int n3 = userInput.nextInt();
-		
-		//Begin Arrays
-		
-		//Create a new array of 3 ints
-		int[] numbers = new int[3];
-		
-		/*
-		Arrays have an index that start with 0
-		and end with their length minus 1 such that
-		an array of 3 ints will have an index from 0-2
-		*/
-		
-		//Here we assign values to each index
-		numbers[0] = n1;
-		numbers[1] = n2;
-		numbers[2] = n3;
-		
-		
-		
-		//And output them with a for loop
-		for(int i=0;i < numbers.length; i++){
-			System.out.println(numbers[i]);
+
+		public class sort3numbers {
+			public static void main(String[] args) {
+				System.out.print("Hello class, "); 
+				System.out.println("this is a sorting algorithm using arrays.");
+				
+				// call the sorting algorithm
+				SortAlgorithm();
+				}
+			public static void SortAlgorithm() {
+
+				// generate a set of random numbers to sort
+				int listSize = 6;
+				int[] numberArray = new int[listSize];
+				numberArray = buildRandomArray(listSize);
+				System.out.print("Our random array is: ( ");
+				for (int i = 0; i < listSize; i++ ){
+					System.out.print(numberArray[i] + " ");
+				}
+				System.out.println(").");
+				
+				
+				// sort our array of numbers
+				numberArray = sortOurArray(numberArray, listSize);
+				System.out.print("Our sorted array is: ( ");
+				for (int i = 0; i < listSize; i++ ){
+					System.out.print(numberArray[i] + " ");
+				}
+				System.out.println(").");
+				
+			}
+			public static int[] buildRandomArray(int listSize) {
+				int[] numberArray = new int[listSize];
+				// populate the array
+				numberArray = populateRandomArray(numberArray, listSize);
+				
+				// Test the array to see if there are duplicates.
+				
+				return numberArray;
+			}
+			public static int[] populateRandomArray(int[] numberArray, int listSize) {
+				
+				int randomRange = listSize * 1000;
+				for (int i = 0; i < listSize; i++ ){
+					numberArray[i] = (int)(Math.random()*randomRange);
+				}
+				return numberArray;
+			}
+			
+			public static int[] sortOurArray(int[] numberArray, int listSize) {
+				
+				int holder_1;
+				int holder_2;
+				for (int passCounter = 1; passCounter <= listSize; passCounter++ ){
+					
+					for (int i = 0; i < listSize-1; i++ ){
+						
+						holder_1 = numberArray[i];
+						holder_2 = numberArray[i+1];
+						
+						if(holder_2 < holder_1){
+							numberArray[i] = holder_2;
+							numberArray[i+1] = holder_1;
+							/* Remove our printing block
+							System.out.print("We had a switch, the new array is: /n( ");
+							
+							for(int j = 0; j < listSize; j++){
+								
+								if (j == i){
+									System.out.print(" " + numberArray[j] + " ");
+								}else if (j == (i+1)){
+									System.out.print(" " + numberArray[j] + " ");
+								}else{
+									System.out.print(numberArray[j] + "  ");
+								}
+							}
+							System.out.println(")");
+							*/
+						}
+						
+					}
+				}
+				return numberArray;
+			}
 		}
-	}
-
-	}
-
-
